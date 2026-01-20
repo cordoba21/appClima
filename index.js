5cba43ed366a7192344e049c8185dfec62c73fca
cordoba21 <angelopolo12345@gmail.com>
2026-01-19 18:16:18 -0600

    V2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const APP_ID = '4090239d69cdb3874de692fd18539299';
 
 const fetchData = position => {
     const { latitude, longitude } = position.coords;
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${APP_ID}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${APP_ID}`)
         .then(response => response.json())
         .then(data => setWeatherData(data));
 }
